be07264e42c3c79f1f43f343e6b49e6ff30ebe05
Marcos Lee <marcosl8686@gmail.com>
2017-04-23 19:37:03 -0700

    update portfolio

---
+++ asset/javascript/script.js
@@ -0,0 +1,10 @@
+$("#content1").hide();
+
+$("#findOut").on("click", function(){
+	$("#content1").show();
+
+})
+$(".navBtn").on("click", function(){
+	$("#content1").show();
+
+})
---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+	var slides = $(".slideshow>li");
+	var slideCount = 0;
+	var totalSlides = slides.length;
+	var slideCache = [];
+})
